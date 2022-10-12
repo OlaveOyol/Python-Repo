874382ce95b61f66b618494eb5ebdf9bbf9eee7b
OlaveOyol <104796769+OlaveOyol@users.noreply.github.com>
2022-10-12 16:03:23 +0100

    - Fixed Fruit Spawning half in the grid - Added Length to the snake - Added Ability to eat Fruit - Added Can Die By Eating Self - Kring

--- Learn-Python-Pygame/main.py
+++ Learn-Python-Pygame/main.py
@@ -5,7 +5,6 @@ import random
 pygame.init()
 dis_w,dis_h = 800,600
 dis=pygame.display.set_mode((dis_w,dis_h))
-pygame.display.update()
 pygame.display.set_caption("Test Snake Game")
 
 colours = {
@@ -16,9 +15,16 @@ colours = {
     "Black" : (0,0,0)
 }
 
+s_colour = (0,0,255)
+s_size = 20
+s_speed = 15
 
 clock = pygame.time.Clock()
 font_style = pygame.font.SysFont(None, 50)
+
+def snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, colours["Green"], [x[0], x[1], snake_block, snake_block])
 
 def message(msg,color):
     mesg = font_style.render(msg, True, color)
@@ -28,20 +34,17 @@ def gameLoop():
     game_over = False
     game_close = False
 
-
-    s_colour = (0,0,255)
-    s_size = 10
-    s_speed = 25
-
-
     x_snake = dis_w/2
     y_snake = dis_h/2
 
     x_snake_change = 0
     y_snake_change = 0
 
-    x_food = round(random.randrange(0, dis_w - s_size) / 10.0) * 10.0
-    y_food = round(random.randrange(0, dis_w - s_size) / 10.0) * 10.0
+    x_food = round(random.randrange(0, dis_w - s_size) / s_size) * s_size
+    y_food = round(random.randrange(0, dis_h - s_size) / s_size) * s_size
+
+    snake_List = []
+    s_length = 1
 
     while not game_over:
     
@@ -50,16 +53,16 @@ def gameLoop():
             message("You Lost! Press Q-Quit or C-Play Again", colours["Red"])
             pygame.display.update()
     
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    game_over = True
-                    game_close = False
-                elif event.key == pygame.K_c:
-                    gameLoop()
-                elif event.key == pygame.K_ESCAPE:
-                    game_over = True
-                    game_close = False
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    elif event.key == pygame.K_c:
+                        gameLoop()
+                    elif event.key == pygame.K_ESCAPE:
+                        game_over = True
+                        game_close = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -83,18 +86,33 @@ def gameLoop():
 
 
         if x_snake >= dis_w or x_snake < 0 or y_snake >= dis_h or y_snake < 0:
-            game_over = True
+            game_close = True
 
         x_snake += x_snake_change
         y_snake += y_snake_change
         dis.fill(colours["Black"])
 
-        pygame.draw.circle(dis,colours["Red"],(x_food,y_food),int(s_size*0.4))
-        pygame.draw.rect(dis,s_colour,[x_snake,y_snake,s_size,s_size])
+        pygame.draw.rect(dis,colours["Red"],[x_food,y_food,s_size,s_size])
+        snake_Head = []
+        snake_Head.append(x_snake)
+        snake_Head.append(y_snake)
+        snake_List.append(snake_Head)
+        if len(snake_List) > s_length:
+            del snake_List[0]
+ 
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+ 
+        snake(s_size, snake_List)
+
         pygame.display.update()
 
         if x_snake == x_food and y_snake == y_food:
-            print("Yummy!!")
+            x_food = round(random.randrange(0, dis_w - s_size) / s_size) * s_size
+            y_food = round(random.randrange(0, dis_h - s_size) / s_size) * s_size
+            s_length += 1
+
         clock.tick(s_speed)
 
     pygame.quit()
